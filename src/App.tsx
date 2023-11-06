e451504f07d22d8a8d5275f723445fbdf10570f1
Hugo-Farias <hg80rod@gmail.com>
2023-11-06 15:32:47 -0300

    mobile main page done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
-import "./App.css";
+import "./App.scss";
+import illu from "./assets/images/illustration-sign-up-mobile.svg";
 
 function App() {
-  return null;
+  return (
+    <div className={"app"}>
+      <img alt={"sign-up mobile illustration"} src={illu} />
+      <div className={"content"}>
+        <h1>Stay updated!</h1>
+        <p>Join 60,000+ product manager receiving monthly updates on:</p>
+        <ul>
+          <li>Product discovery and building what matters</li>
+          <li>Measuring to ensure updates are a success</li>
+          <li>And much more!</li>
+        </ul>
+        <div className={"input-container"}>
+          <input
+            alt={"Email address"}
+            type={"email"}
+            placeholder={"email@company.com"}
+          />
+        </div>
+        <button>Subscribe to monthly newsletter</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
